cfe3382b2741c2d9e717cd769ef8bba28e9091a7
Eric Calabretta <ericcalabretta@users.noreply.github.com>
2018-06-06 08:18:40 -0500

    Update plan.ps1

--- hab-sln/habitat/plan.ps1
+++ hab-sln/habitat/plan.ps1
@@ -1,7 +1,6 @@
 $pkg_name="hab-sln"
-$pkg_origin="mwrock"
+$pkg_origin="ericcalabretta"
 $pkg_version="0.1.0"
-$pkg_source="nosuchfile.tar.gz"
 $pkg_maintainer="Matt Wrock"
 $pkg_license=@('MIT')
 $pkg_description="A sample ASP.NET Full FX IIS app"
